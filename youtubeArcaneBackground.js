de2f31f07c843ac7d0c7b4557ed941c410f2e054
arcaneless <moddcheung88819@gmail.com>
2019-12-12 15:03:50 +0800

    done fading of dots TODO: connecting dots with line

--- youtubeArcaneBackground.js
+++ youtubeArcaneBackground.js
@@ -8,51 +8,101 @@ canvas.width  = window.innerWidth;
 canvas.height = window.innerHeight;
 
 //Done! Enjoy full page canvas!
-var speed = 100; // pixel/ms
+var speed = 20; // pixel/ms
 var distance = 300; // pixel
+var fadingFactor = 0.2; // 0-1
+var connectingFactor = 10; // >1
 function Dot(fabricObject, duration) {
     this.fabricObject = fabricObject;
     this.duration = duration;
+    this.life = 0;
+    this.lifeEnd = false;
+    this.fadeOut = false;
 }
 var dots = [];
 var canvas = new fabric.Canvas('c', { renderOnAddRemove: false });
 fabric.Object.prototype.transparentCorners = false;
 
+function easeLinear (t, b, c, d) {
+    return b + (t/d) * c;
+}
+
 function initialize() {
     for (let i=0; i<50; i++) {
-        let o = new fabric.Circle({
-            radius: 5,
-            fill: 'black',
-            selectable: false,
-            left: window.innerWidth * Math.random(),
-            top: window.innerHeight * Math.random(),
-            objectCaching: false
+        addDot();
+    }
+}
+
+function addDot() {
+    let o = new fabric.Circle({
+        radius: 5,
+        fill: 'black',
+        opacity: 0,
+        selectable: false,
+        left: window.innerWidth * Math.random(),
+        top: window.innerHeight * Math.random(),
+        objectCaching: false,
+        easing: easeLinear
+    });
+    let dot = new Dot(o, 1000 * (distance * (Math.random() + 0.5)) / speed);
+    dots.push(dot);
+
+
+    let angle = Math.random() * Math.PI * 2;
+    dot.fabricObject.animate(
+        {left: dot.fabricObject.left + (distance * Math.cos(angle)),
+            top:  dot.fabricObject.top + (distance * Math.sin(angle))},
+        {
+            duration: dot.duration,
         });
-        dots.push(new Dot(o, 1000 * distance / speed));
+    dot.fabricObject.animate(
+        'opacity', 1,
+        {
+            duration: dot.duration * fadingFactor,
+        });
+
+    canvas.add(dot.fabricObject);
+}
+
+function onChangeDot(dot) {
+    dot.life += 10;
+    if (dot.life >= dot.duration) {
+        dot.lifeEnd = true;
+        canvas.getObjects().forEach((obj) => {
+            if (obj === dot.fabricObject) {
+                canvas.remove(obj);
+            }
+        });
+        for (let i = 0; i < dots.length; i++) {
+            if (dots[i] === dot) {
+                dots.slice(i, 1);
+                addDot();
+                return;
+            }
+        }
+    }
+    if ((dot.life >= (dot.duration * (1 - fadingFactor))) && !dot.fadeOut) {
+        dot.fadeOut = true;
+        dot.fabricObject.animate(
+            'opacity', 0,
+            {
+                duration: dot.duration * fadingFactor /2,
+            });
     }
 }
 
 function animate() {
-    for (let dot of dots) {
-        let angle = Math.random() * Math.PI * 2;
-        dot.fabricObject.animate(
-            {left: dot.fabricObject.left + (distance * Math.cos(angle)),
-             top:  dot.fabricObject.top + (distance * Math.sin(angle))},
-            {
-                duration: dot.duration,
-            })
-    }
     setTimeout(motionLoop, 10);
 }
 
 function motionLoop() {
     canvas.renderAll();
+    for (let dot of dots) {
+        if (!dot.lifeEnd) onChangeDot(dot);
+    }
     setTimeout(motionLoop, 10);
 }
 
 
 initialize();
-for (let dot of dots) {
-    canvas.add(dot.fabricObject);
-}
 animate();
